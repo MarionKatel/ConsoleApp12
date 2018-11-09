7ecedd7ed3956e98904199b551d877c8f6f8b235
MarionKatelAdministrateur <Administrateur@UTILISA-KU9GGSU>
2018-11-09 14:10:39 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,56 @@
+﻿using ExceptionStudentList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StudentListIsNotFoundException ret = new StudentListIsNotFoundException();
+                    throw  ret;
+           
+            }
+       /* {
+            short y = 1;
+
+
+            try
+            {
+                int u = Convert.ToInt32(y);
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (StudentListIsNotFoundException str) { }
+            
+
+            try
+            {
+                
+              int z = 8;
+             }*/
+
+
+                catch (StudentListIsNotFoundException stuEx)
+                {
+                Console.WriteLine( stuEx.Message);
+
+                }
+            finally
+            {
+                Console.ReadLine();
+              
+            }
+            }
+        }
+
+    }
+
---
+++ ConsoleApp12/StudentListIsNotFoundException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionStudentList
+
+{
+    public class StudentListIsNotFoundException : Exception
+    {
+        public StudentListIsNotFoundException()
+        {
+            throw new StudentListIsNotFoundException("");
+        }
+        public StudentListIsNotFoundException(string message)
+            : base("List student not found")
+        {
+
+        }
+
+        public StudentListIsNotFoundException(string message, Exception inner)
+        : base(message, inner)
+            {
+
+
+            }
+        
+    }
+}
+  
+
+
